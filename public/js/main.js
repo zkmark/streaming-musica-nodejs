8e6af4706f6365af203472c6a57bd05b34718f1e
mark <xeneas@hotmail.com>
2016-08-28 05:37:01 -0500

    09 - Conectar API a la interfaz

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-console.log('estoy en browser');
+$(function () {
+	function cargarCanciones(){
+		//Con ajax pasamos un objeto
+		$.ajax({
+			url: '/canciones'
+		//Recibimos array de canciones
+		}).done(function(canciones){
+			var lista = $('.lista-canciones');
+			//Vaciamos la lista para agregar nuestras canciones
+			lista.empty();
+			//Desplegamos el array
+			canciones.forEach(function(cancion){
+				var nuevoElemento = $('<li class="cancion">'+cancion.nombre+'</li>');
+				//Agregamos el nuevo elemento a la lista
+				nuevoElemento.appendTo(lista);
+			})
+		//Si falla
+		}).fail(function(){
+			alert('No se pudieron cargar las canciones');
+		})
+	}
+
+	cargarCanciones();
+});
