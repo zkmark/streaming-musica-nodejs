1db1bd4cd5a8ac25fb99b38f7459c9c9f63c9b74
mark <xeneas@hotmail.com>
2016-08-28 06:20:01 -0500

    11 - Implementar cambio de canción

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+	var audio = $('audio');
+	
 	function cargarCanciones(){
 		//Con ajax pasamos un objeto
 		$.ajax({
@@ -11,13 +13,26 @@ $(function () {
 			//Desplegamos el array
 			canciones.forEach(function(cancion){
 				var nuevoElemento = $('<li class="cancion">'+cancion.nombre+'</li>');
-				//Agregamos el nuevo elemento a la lista
-				nuevoElemento.appendTo(lista);
+				
+				nuevoElemento
+					//Cuando se de clic, obtenemos la cancion y activamos la funcion play
+					.on('click', cancion, play)
+					//Agregamos el nuevo elemento a la lista
+					.appendTo(lista);
 			})
 		//Si falla
 		}).fail(function(){
 			alert('No se pudieron cargar las canciones');
 		})
+	}
+
+	//Cambiara el atributo src
+	function play(evento){
+		//Elemento nativo audio[0] , para la cancion
+		audio[0].pause();
+		//Elemento Jquery audio , data nos da acceso al objeto cancion (al que se clickeo)
+		audio.attr('src', '/canciones/' + evento.data.nombre);
+		audio[0].play();
 	}
 
 	cargarCanciones();
